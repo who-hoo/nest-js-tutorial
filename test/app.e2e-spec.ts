04587a1bed4b7729004169cc89800b7769669eeb
who-hoo <who.ho3ov@gmail.com>
2020-12-22 22:31:52 +0900

    2020.12.22
    나머지 요청에 대한 e2e 테스트 코드 작성

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -29,13 +29,13 @@ describe('AppController (e2e)', () => {
   });
 
   describe('/movies', () => {
-    it('GET', () => {
+    it('GET 200', () => {
       return request(app.getHttpServer())
         .get('/movies')
         .expect(200)
         .expect([]);
     });
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -45,7 +45,7 @@ describe('AppController (e2e)', () => {
         })
         .expect(201);
     });
-    it('DELETE', () => {
+    it('DELETE 404', () => {
       return request(app.getHttpServer())
         .delete('/movies')
         .expect(404);
@@ -63,7 +63,27 @@ describe('AppController (e2e)', () => {
         .get('/movies/999')
         .expect(404);
     });
-    it.todo('DELETE');
-    it.todo('PATCH');
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated Test' })
+        .expect(200);
+    });
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['Test'],
+          other: 'thing',
+        })
+        .expect(400);
+    });
+    it('DELETE 200', () => {
+      return request(app.getHttpServer())
+        .delete('/movies/1')
+        .expect(200);
+    });
   });
 });
